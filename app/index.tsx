1bb701f49a6d06e6f2ce3c4a0bb4ba4ac27cb45d
Bryan Lee <bryanrichie97@gmail.com>
2024-11-28 13:47:59 +1100

    feat: created dashboard design and components

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,5 @@
+import { Dashboard } from '@/components/Dashboard';
+import { Sidebar } from '@/components/Sidebar';
 import { Text, View } from 'react-native';
 
 export default function Index() {
@@ -5,11 +7,12 @@ export default function Index() {
     <View
       style={{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        flexDirection: 'row',
+        marginRight: 50,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen. Hello World</Text>
+      <Sidebar />
+      <Dashboard />
     </View>
   );
 }
